6edfa8320c611a6fedf064ba42a6547d2e4dccd9
Liang Chen <chen348@indiana.edu>
2016-04-05 01:23:38 -0400

    training hog+linear svm

--- src/__main__.py
+++ src/__main__.py
@@ -2,7 +2,7 @@
 # main function, a quick testing
 
 from pdfReader import PdfReader
-from symbolDetector import SymbolDetector
+from symbolDetector import SymbolDetector, DetectorOption
 from train import training
 
 if "__main__" == __name__:
@@ -12,5 +12,6 @@ if "__main__" == __name__:
     except Exception:
         print Exception
 
-    training("/Users/Hipapa/Projects/Git/Vintager/data/train0.jpg", "/Users/Hipapa/Projects/Git/Vintager/annotations/train.ant", "hog")
-    #    sd = SymbolDetector(None)
+    #training("/Users/Hipapa/Projects/Git/Vintager/data/train0.jpg", "/Users/Hipapa/Projects/Git/Vintager/annotations/train.ant", "hog")
+    option = DetectorOption("hog_svm")
+    sd = SymbolDetector(option)

ece7d18dddcc0ecf7ded0ed2919da8af893c49fc
Liang Chen <chen348@indiana.edu>
2016-04-14 14:44:44 -0400

    reformat the code with better encapsulation

--- src/SymbolDetector.py
+++ src/SymbolDetector.py
@@ -1,60 +1,59 @@
 
 # detect various types of music symbols via different detectors
-# 1, HOG + Muticlass-SVM
-# 2, Convolutional Neural Network
+# 1, Pixel + Multiclass-SVM
+# 2, HOG + Muticlass-SVM
+# 3, Convolutional Neural Network
 
 import cv2
 import numpy as np
 from sklearn.externals import joblib
 from glob import *
-from utils import hog
+#from utils import create_symbol
+from feature import hog, pixel_vec
 
 
 class DetectorOption:
     def __init__(self, name):
-        self.name = name
+        self.__name__ = name
+
+    def name(self):
+        return self.__name__
 
 
 class SymbolDetector:
     def __init__(self, option):
         try:
-            if option.name == "hog_svm":
-                dir = '../models/'
-                self.model = joblib.load(dir + option.name + '.pkl')
-                #self.extractor = cv2.HOGDescriptor(dir + "hog.xml")
+            model_dir = '../models/'
+            self.model = joblib.load(model_dir + option.name() + '.pkl')
+            if option.name() == "hog_svm":
                 self.extractor = hog
+            elif option.name() == "pixel_svm":
+                self.extractor = pixel_vec
 
         except Exception:
+            print "detector initialization"
             print Exception
-
 
     def detect(self, im, label, mode):
         tot_rows, tot_cols = im.shape[:2]
         [rows, cols] = symbol_label_parms[label]
         detected = []
-        rows = int(rows)
-        cols = int(cols)
-        sample_step = 2
-        for i in range(0, tot_rows - rows, sample_step):
-            for j in range(0, tot_cols - cols, sample_step):
+        step_size = 2
+        for i in range(0, tot_rows - rows, step_size):
+            for j in range(0, tot_cols - cols, step_size):
                 sub_im = im[i:i+rows, j:j+cols]
                 sub_im = cv2.resize(sub_im, uni_size)
-                #if self.model.predict(self.extractor.compute(sub_im, None, None, ((0,0),)).reshape(-1,1764)) == 1:
                 feature = self.extractor(sub_im)
                 feature = feature.reshape(-1,feature.size)
-                #print i,j
-                #print self.model.predict_proba(feature)[0][0], self.model.predict_proba(feature)[0][1]
-                #if 10*self.model.predict_proba(feature)[0][0] <= self.model.predict_proba(feature)[0][1]:
                 if self.model.predict(feature) == label:
                     detected.append((i,j))
-                    #print i,j
 
         if mode == "show":
             rgb_im = cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
             font = cv2.FONT_HERSHEY_SIMPLEX
             for (i, j) in detected:
-                cv2.rectangle(rgb_im, (j, i), (j + cols, i + rows), (0, 255, 0), 2)
-                #cv2.putText(rgb_im, label, (j - 10, i), font, 1, (0, 255, 0), 2)
+                cv2.rectangle(rgb_im, (j, i), (j + cols, i + rows), (0, 0, 255), 2)
+                cv2.putText(rgb_im, label, (j, i - 10), font, 0.4, (0, 0, 255), 1)
             cv2.imwrite(label+".jpg", rgb_im)
             cv2.imshow("image", rgb_im)
             cv2.waitKey(0)
@@ -66,41 +65,47 @@ class SymbolDetector:
     def detect_all(self, im, mode):
         tot_rows, tot_cols = im.shape[:2]
         detected = []
-        step_size = 3
+        step_size = 10
         cls = self.model.classes_
         for i in xrange(0, tot_rows, step_size):
             for j in xrange(0, tot_cols, step_size):
-                prob = 0
-                pred_label = "background"
-                print i,j
                 for label in symbol_label_parms.keys():
                     [rows, cols] = symbol_label_parms[label]
-                    rows = int(rows)
-                    cols = int(cols)
                     if i + rows >= tot_rows or j + cols >= tot_cols:
                         continue
                     sub_im = im[i:i + rows, j:j + cols]
                     sub_im = cv2.resize(sub_im, uni_size)
                     feature = self.extractor(sub_im)
                     feature = feature.reshape(-1, feature.size)
-                    temp_prob = self.model.predict_proba(feature)[0][np.where(cls == label)]
-                    if temp_prob > prob:
-                        prob = temp_prob
-                        pred_label = label
+                    if self.model.predict(feature) == label:
+                        detected.append((self.model.predict_proba(feature)[0][np.where(cls == label)], i, j, label))
+                    # how to get comparable score here???
+                    #temp_prob = self.model.predict_proba(feature)[0][np.where(cls == label)]
 
-                if pred_label != "background":
-                    detected.append((i, j, pred_label))
-                    # print i,j
+        ##Non-Maxima Suppression
+        detected.sort(key=lambda tup: tup[0])
+        hashed = np.zeros((tot_rows, tot_cols), dtype=bool)
+        suppressed = []
+        for (score, i, j, label) in detected:
+            if label == "background" or hashed[i][j]:
+                continue
+            suppressed.append((i,j,label))
+            print i,j
+            hashed[max(0, i - default_symbol_rows / 2): min(i + default_symbol_rows / 2, tot_rows - 1)][
+            max(0, j - default_symbol_cols / 2): min(j + default_symbol_cols / 2, tot_cols - 1)] = True
+            #[rows, cols] = symbol_label_parms[label] #which suppression region should I use? the symbol bbox or the default bbox?
 
         if mode == "show":
             rgb_im = cv2.cvtColor(im, cv2.COLOR_GRAY2RGB)
             font = cv2.FONT_HERSHEY_SIMPLEX
-            for (i, j, label) in detected:
+
+            for (i, j, label) in suppressed:
                 [rows, cols] = symbol_label_parms[label]
                 rows = int(rows)
                 cols = int(cols)
                 cv2.rectangle(rgb_im, (j, i), (j + cols, i + rows), (0, 0, 255), 2)
-                cv2.putText(rgb_im, label, (j - 20, i - 10), font, 0.4, (0, 0, 255), 1)
+                cv2.putText(rgb_im, label, (j, i - 10), font, 0.4, (0, 0, 255), 1)
+
             cv2.imwrite('detected.jpg', rgb_im)
             cv2.imshow("image", rgb_im)
             cv2.waitKey(0)
--- src/feature.py
+++ src/feature.py
@@ -6,17 +6,23 @@ import numpy as np
 
 
 def hog(img):
-    bin_n = 16  # Number of bins
-    gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
-    gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
-    mag, ang = cv2.cartToPolar(gx, gy)
-    bins = np.int32(bin_n*ang/(2*np.pi))    # quantizing bin values in (0...16)
-    bin_cells = bins[:10,:10], bins[10:,:10], bins[:10,10:], bins[10:,10:]
-    mag_cells = mag[:10,:10], mag[10:,:10], mag[:10,10:], mag[10:,10:]
-    hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
-    hist = np.hstack(hists)     # hist is a 64 bit vector
-    hist /= 1000.0
-    return hist
+
+    #hijacking hog with pixel features for now
+    feature = np.squeeze(img.reshape(-1, img.size) / 256.0)  # using pixel features
+    return feature
+
+
+    # bin_n = 16  # Number of bins
+    # gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+    # gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+    # mag, ang = cv2.cartToPolar(gx, gy)
+    # bins = np.int32(bin_n*ang/(2*np.pi))    # quantizing bin values in (0...16)
+    # bin_cells = bins[:10,:10], bins[10:,:10], bins[:10,10:], bins[10:,10:]
+    # mag_cells = mag[:10,:10], mag[10:,:10], mag[:10,10:], mag[10:,10:]
+    # hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
+    # hist = np.hstack(hists)     # hist is a 64 bit vector
+    # hist /= 1000.0
+    # return hist
 
 
 def pixel_vec(img):

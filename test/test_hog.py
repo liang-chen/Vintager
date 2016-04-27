a2ada936d29a33f9c7ae630ae2383541cf0e2bbb
Liang Chen <chen348@indiana.edu>
2016-04-26 20:53:55 -0400

    add unitest for hog feature detection

--- test/test_hog.py
+++ test/test_hog.py
@@ -1,13 +1,9 @@
 
-import unittest
 from unittest import TestCase
 from feature import hog
-import cv2
 import numpy as np
+
 
 class TestHog(TestCase):
     def test_hog(self):
         self.assertTrue(hog, np.zeros((50, 50), np.uint8))
-
-if __name__ == '__main__':
-    unittest.main()

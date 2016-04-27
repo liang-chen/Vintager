8ad22735c804ab9eac626dd01b6cfe2911c6b444
Liang Chen <chen348@indiana.edu>
2016-04-26 20:53:15 -0400

    add unitest for hog feature detection

--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ import cv2
 import numpy as np
 import random
 from globv import *
-from symbol import Symbol, LOC, BBox
+from symbol import Symbol, LOC
 from sklearn.svm import SVC
 from sklearn.externals import joblib
 from utils import create_symbol

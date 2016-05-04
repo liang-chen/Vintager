9446d242ae324d5d576e414167bb0245980f2b7d
Liang Chen <chen348@indiana.edu>
2016-05-03 22:57:05 -0400

    use 4*4 max pooling instead of 2*2 pooling

--- src/cnn.py
+++ src/cnn.py
@@ -25,8 +25,8 @@ def conv2d(x, W):
 
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 4, 4, 1],
+                        strides=[1, 4, 4, 1], padding='SAME')
 
 
 def get_data_batch(i, batch_size):
@@ -110,33 +110,45 @@ def train_cnn():
     h_pool2 = max_pool_2x2(h_conv2)
 
     ##third convolutional layer
-    W_conv3 = weight_variable([5, 5, 64, 128])
-    b_conv3 = bias_variable([128])
-    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
-    h_pool3 = max_pool_2x2(h_conv3)
+    # W_conv3 = weight_variable([5, 5, 64, 128])
+    # b_conv3 = bias_variable([128])
+    # h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
+    # h_pool3 = max_pool_2x2(h_conv3)
 
     ##fully connected layer
-    W_fc1 = weight_variable([8 * 8 * 128, 1024])
+    W_fc1 = weight_variable([4 * 4 * 64, 1024])
     b_fc1 = bias_variable([1024])
-    h_pool3_flat = tf.reshape(h_pool3, [-1, 8 * 8 * 128])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 4 * 4 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    ##fully connected layer
+    # W_fc2 = weight_variable([2048, 1024])
+    # b_fc2 = bias_variable([1024])
+    # h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
 
     ##Readout layer
-    W_fc2 = weight_variable([1024, n_labels])
-    b_fc2 = bias_variable([n_labels])
-    y_conv = tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2)
+    W_fc3 = weight_variable([1024, n_labels])
+    b_fc3 = bias_variable([n_labels])
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1, W_fc3) + b_fc3)
 
     #print "okay"
     cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
     correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    find_y_conv = tf.argmax(y_conv,1)
     sess.run(tf.initialize_all_variables())
     for i in range(2000):
         #print i
         batch = get_data_batch(i,batch_size)
+
         if i % 20 == 0:
             train_accuracy = accuracy.eval(feed_dict={
+                x: batch[0], y_: batch[1]})
+            #print batch[1]
+            #cv2.imshow("test", batch[0][0].reshape(64,64))
+            #cv2.waitKey(0)
+            print find_y_conv.eval(feed_dict={
                 x: batch[0], y_: batch[1]})
             print("step %d, training accuracy %g" % (i, train_accuracy))
         train_step.run(feed_dict={x: batch[0], y_: batch[1]})

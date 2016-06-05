102d0915cafeb166c233abe25e72e6f2cd4c706b
Liang Chen <chen348@indiana.edu>
2016-06-04 23:11:14 -0400

    update cnn detection results

--- demo/detect_symbol_cnn_demo.py
+++ demo/detect_symbol_cnn_demo.py
@@ -5,7 +5,7 @@ Demo: Detect symbol via convolutional neural network
 
 from symbolDetector import DetectorOption, SymbolDetector
 from pdfReader import PdfReader
-import cv2
+
 
 if "__main__" == __name__:
     data_dir = '../data/'
@@ -15,6 +15,4 @@ if "__main__" == __name__:
 
     option = DetectorOption("cnn")
     sd = SymbolDetector(option)
-    #cv2.imshow("haha", pr.page(0)[180:280, 20:300])
-    #cv2.waitKey(0)
-    sd.detect_all(pr.page(0)[180:280, 20:300], "show")
+    sd.detect_all(pr.page(0)[0:500, 0:500], "show")
